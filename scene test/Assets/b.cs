ddde8527c382e7e53854e2fbf7c7fe1d274cb663
abcpng <54026737+abcpng@users.noreply.github.com>
2019-08-12 21:52:15 +0900

    5

--- scene test/Assets/b.cs
+++ scene test/Assets/b.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class b : MonoBehaviour
 {
+
+    public int n=2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        n = 5;
     }
 
     // Update is called once per frame
