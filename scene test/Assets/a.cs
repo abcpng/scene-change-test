14ee3d86cebd35561a4f8e868c9da832dce3f7fb
kim96-2 <53760390+kim96-2@users.noreply.github.com>
2019-08-12 21:51:37 +0900

    4

--- scene test/Assets/a.cs
+++ scene test/Assets/a.cs
@@ -7,7 +7,7 @@ public class a : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Rigidbody>().AddForce(Vector3.forward*10f, ForceMode.Impulse);
     }
 
     // Update is called once per frame
